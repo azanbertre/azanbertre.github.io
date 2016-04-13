82ce00981721ecb639f930ff5040b5e68bf1669e
Pedro Henrique de O. V. Borges <pedro.hov.borges@gmail.com>
2016-04-13 00:32:27 -0300

    Added some js (fade effects)

---
+++ scripts/fade.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("div").fadeOut({queue: false, duration: 0});
+	$("topbar").fadeIn({queue: false, duration: "fast"});
+	$("div").fadeIn({queue: false, duration: "slow"});
+});
